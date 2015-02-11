eb6a203f5087a857e26e206b80be7549bc876d43
ajlopez <webmaster@ajlopez.com>
2015-02-11 17:37:36 -0300

    Register and execute first verb

--- test/dsl.js
+++ test/dsl.js
@@ -7,3 +7,16 @@ exports['Create dsl'] = function (test) {
     test.ok(dsl);
     test.equal(typeof dsl, 'object');   
 }
+
+exports['Register and execute verb'] = function (test) {
+    test.async();
+    
+    var dsl = sdsl.dsl();
+    
+    dsl.register('foo', function (cmd, cb) { cb(null, 1); });
+    dsl.execute('foo', function (err, data) {
+        test.equal(err, null);
+        test.equal(data, 1);
+        test.done();
+    });
+}
