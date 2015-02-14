3b0ab1fdf95301711d3c864d59977997dabc741a
ajlopez <webmaster@ajlopez.com>
2015-02-14 16:33:35 -0300

    Receives command with arguments

--- test/dsl.js
+++ test/dsl.js
@@ -52,3 +52,26 @@ exports['Receives command with verb'] = function (test) {
         test.done();
     });
 }
+
+exports['Receives command with arguments'] = function (test) {
+    test.async();
+    
+    var dsl = sdsl.dsl();
+    
+    dsl.register('foo', function (cmd, cb) { 
+        test.ok(cmd);
+        test.ok(cmd.verb);
+        test.equal(cmd.verb, 'foo');
+        test.ok(cmd.args);
+        test.equal(cmd.args.length, 2);
+        test.equal(cmd.args[0], 'arg1');
+        test.equal(cmd.args[1], 'arg2');
+        cb(null, 1); 
+    });
+    
+    dsl.execute('foo arg1 arg2', function (err, data) {
+        test.equal(err, null);
+        test.equal(data, 1);
+        test.done();
+    });
+}
