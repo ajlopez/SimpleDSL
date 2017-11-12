4605284a042496e7a21b7a3d6b0274d31a5bf545
ajlopez <webmaster@ajlopez.com>
2017-11-12 18:57:21 -0300

    Updated stack sample

--- samples/stack/run.js
+++ samples/stack/run.js
@@ -14,17 +14,29 @@ dsl
 .define('pop', function (cmd, next) {
 	next(null, stack.pop());
 })
+.define('dup', function (cmd, next) {
+	var value = stack[stack.length - 1];
+	stack.push(value);
+	next(null, value);
+})
+.define('add', function (cmd, next) {
+	next(null, stack.push(stack.pop() + stack.pop()));
+})
 .define('dump', function (cmd, next) {
 	console.dir(stack);
 	next(null, null);
 })
 
 function evaluate(cmd) {
-	return eval(cmd.argstext());
+	return eval(cmd.argumentsText());
 }
 
-process.argv.slice(2).forEach(function (filename) {
-	dsl.executeFile(filename);
+var filename = process.argv[2];
+
+dsl.executeFile(filename, function (err, data) {
+	if (err)
+		console.error(err);
 });
+
 
 
