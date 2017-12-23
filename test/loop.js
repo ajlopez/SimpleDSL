32a78c2058c9c8e6f2658a07968b51e703e2087f
ajlopez <webmaster@ajlopez.com>
2017-12-23 14:43:54 -0300

    Loop command

---
+++ test/loop.js
@@ -0,0 +1,39 @@
+
+var sdsl = require('..');
+var path = require('path');
+
+exports['Define and execute loop verb returning false'] = function (test) {
+    test.async();
+    
+    var dsl = sdsl.dsl();
+    
+    dsl
+		.define('while', function (cmd, cb) { cb(null, eval(cmd.argumentsText())); }, { loop: true })
+		.define('fail', function (cmd, cb) { test.fail(); })
+		.define('end', { close: true });
+
+	dsl.execute([ 'while false', 'fail', 'end' ], function (err, data) {
+        test.equal(err, null);
+        test.done();
+    });
+}
+
+exports['Define and execute loop verb many times'] = function (test) {
+    test.async();
+    
+    var dsl = sdsl.dsl();
+	
+	var x = 0;
+    
+    dsl
+		.define('while', function (cmd, cb) { cb(null, eval(cmd.argumentsText())); }, { loop: true })
+		.define('eval', function (cmd, cb) { cb(null, eval(cmd.argumentsText())); })
+		.define('end', { close: true });
+
+	dsl.execute([ 'while x < 10', 'eval x++', 'end' ], function (err, data) {
+        test.equal(err, null);
+		test.equal(x, 10);
+        test.done();
+    });
+}
+
