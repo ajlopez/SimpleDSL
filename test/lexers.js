1bfaeb1094cb470f0cd3a0efb5289c052ced0d42
ajlopez <webmaster@ajlopez.com>
2017-12-24 11:30:08 -0300

    Argument delimiter in lexer

--- test/lexers.js
+++ test/lexers.js
@@ -42,3 +42,30 @@ exports['get tokens with spaces'] = function (test) {
 	test.equal(tokens[1], 'bar');
 };
 
+exports['get tokens with string'] = function (test) {
+	var lexer = lexers.lexer();
+	
+	var tokens = lexer.getTokens('foo  bar "zoo"');
+	
+	test.ok(tokens);
+	test.ok(Array.isArray(tokens));
+	test.equal(tokens.length, 3);
+	test.equal(tokens[0], 'foo');
+	test.equal(tokens[1], 'bar');
+	test.equal(tokens[2], '"zoo"');
+};
+
+exports['get tokens with argument delimiter'] = function (test) {
+	var lexer = lexers.lexer({ delimiter: ';'});
+	
+	var tokens = lexer.getTokens('foo  one ; two; three');
+	
+	test.ok(tokens);
+	test.ok(Array.isArray(tokens));
+	test.equal(tokens.length, 4);
+	test.equal(tokens[0], 'foo');
+	test.equal(tokens[1], 'one');
+	test.equal(tokens[2], 'two');
+	test.equal(tokens[3], 'three');
+};
+
