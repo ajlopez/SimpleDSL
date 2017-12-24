2f6d7fb4893d5a710ca5b74125f361784e6ac34f
ajlopez <webmaster@ajlopez.com>
2017-12-24 10:55:55 -0300

    Lexer first tests

---
+++ test/lexers.js
@@ -0,0 +1,34 @@
+
+var lexers = require('../lib/lexers');
+
+exports['create lexer as object'] = function (test) {
+	var lexer = lexers.lexer();
+	
+	test.ok(lexer);
+	test.equal(typeof lexer, 'object');
+};
+
+exports['get tokens'] = function (test) {
+	var lexer = lexers.lexer();
+	
+	var tokens = lexer.getTokens('foo bar');
+	
+	test.ok(tokens);
+	test.ok(Array.isArray(tokens));
+	test.equal(tokens.length, 2);
+	test.equal(tokens[0], 'foo');
+	test.equal(tokens[1], 'bar');
+};
+
+exports['get tokens with spaces'] = function (test) {
+	var lexer = lexers.lexer();
+	
+	var tokens = lexer.getTokens('  foo   bar  ');
+	
+	test.ok(tokens);
+	test.ok(Array.isArray(tokens));
+	test.equal(tokens.length, 2);
+	test.equal(tokens[0], 'foo');
+	test.equal(tokens[1], 'bar');
+};
+
